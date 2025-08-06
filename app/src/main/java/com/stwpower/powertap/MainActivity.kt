af8470cb6c857df48dd90e74c2cf2aa072d93d5d
huz <huz.gdhl@gmail.com>
2025-08-06 18:19:18 +0800

    改UI，提交一下先

--- app/src/main/java/com/stwpower/powertap/MainActivity.kt
+++ app/src/main/java/com/stwpower/powertap/MainActivity.kt
@@ -20,6 +20,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
